6d1aa9c70f5c3ab5cf133acdbba8c73d79dc0657
YanAndreatta <yan.duca2@gmail.com>
2024-06-20 20:06:51 -0300

    initial commit

---
+++ desafio_com_funcoes.py
@@ -0,0 +1,18 @@
+def saque(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    return
+
+def deposito(saldo, valor, extrato, /):
+    return saldo, extrato
+
+def extrato(saldo, /, *, extrato):
+    return
+
+def endereco(logradouro, nro, bairro, cidade, sigla_estado):
+    return 
+
+def cria_usuario(nome, nascimento, cpf):
+    return
+
+def cria_conta_corrente(usuario):
+    AGENCIA = "0001"
+    return    
